039ea5edfd95c400342117151a79c1a39a42f36a
Richie <richiecool049@gmail.com>
2024-04-11 19:58:14 +0700

    pppp

---
+++ app/src/main/java/com/example/suksesmotor/LoginPegawai.java
@@ -0,0 +1,14 @@
+package com.example.suksesmotor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginPegawai extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_pegawai);
+    }
+}
